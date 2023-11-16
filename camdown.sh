d238e2a3ef8a6a08cb3a986a1b5399cd119b631c
Twilsonbr549 <mountaingroveproductions@gmail.com>
2023-11-16 12:57:19 -0600

    revised for timelapse removal

--- camdown.sh
+++ camdown.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 _date=$(date +%Y-%m-%d -d "1 day ago")
 echo "Yesterdays Date: "$_date
+echo ""
 for d in *
 do
 	_COUNT=$(ls -aFl "$d" | grep "$_date" | wc -l) 
 	if [ "$_COUNT" == 0 ]
 		then
-			printf %s\\n ""$d" Stopped recording yesterday!!!"
+			if [[ "$d" != *"timelapse"* ]]
+				printf %s\\n ""$d" Stopped recording yesterday!!!"
+			fi
 	fi
 done
 
